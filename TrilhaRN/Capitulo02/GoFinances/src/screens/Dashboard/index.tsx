c5bff920a20d66279c3aacde01c212d564dd7992
JoaoVitorSPereira <joaovitorspereira@gmail.com>
2021-09-30 23:54:32 -0300

    Chapter 02 React-Native

---
+++ TrilhaRN/Capitulo02/GoFinances/src/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Title>BOB</Title>
+    </Container>
+  );
+};
+
+export default Dashboard;
