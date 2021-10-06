76d2140337d6d67f2008918aa9478862c1186378
JoaoVitorSPereira <joaovitorspereira@gmail.com>
2021-10-06 09:57:26 -0300

    Added Scrollview in Cards and estilization

--- TrilhaRN/Capitulo02/GoFinances/src/screens/Dashboard/index.tsx
+++ TrilhaRN/Capitulo02/GoFinances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
-      <Title>BOB</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/57682027?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Ola, </UserGreeting>
+              <UserName>João</UserName>
+            </User>
+          </UserInfo>
+          <Icon name='power' />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type='up'
+          title='Entradas'
+          amount='R$ 17.400,00'
+          lastTransaction='Última entrada dia 13 de abril'
+        />
+        <HighlightCard
+          type='down'
+          title='Saídas'
+          amount='R$ 1.259,00'
+          lastTransaction='Última entrada dia 13 de abril'
+        />
+        <HighlightCard
+          type='total'
+          title='Entradas'
+          amount='R$ 16.141,00'
+          lastTransaction='Última entrada dia 13 de abril'
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+      </Transactions>
     </Container>
   );
 };
