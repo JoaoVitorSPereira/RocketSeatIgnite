cfbd7649496dfd58b772ec01c1e4437bdf1a3d94
JoaoVitorSPereira <joaovitorspereira@gmail.com>
2021-10-22 09:07:43 -0300

    Register page ready, developing CategorySelect page

--- TrilhaRN/Capitulo02/GoFinances/src/screens/Register/styles.ts
+++ TrilhaRN/Capitulo02/GoFinances/src/screens/Register/styles.ts
@@ -33,4 +33,6 @@ export const Title = styled.Text`
 export const TransactionsTypes = styled.View`
   flex-direction: row;
   justify-content: space-between;
+  margin-top: 8px;
+  margin-bottom: 16px;
 `;
